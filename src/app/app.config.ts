2891c1e5b0a566064ef9b00c7d1eacd59bd74af4
Stefan Redl <bernhardlaus@gmx.at>
2025-04-09 19:52:20 +0200

    install Firebase 13.0.1 and Angular 17.0.6 and Angular 17.0.0

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), 
+    importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"notes-888ea","appId":"1:135122385963:web:70a0c20ae1f7b146fafe6e","storageBucket":"notes-888ea.firebasestorage.app","apiKey":"AIzaSyDWSnpneCAVKQfR_R79AecJ5fnn7rqlNL8","authDomain":"notes-888ea.firebaseapp.com","messagingSenderId":"135122385963"}))), 
+    importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
