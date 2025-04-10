da178570d18e5c66825aa00222a1600d66815ee1
Stefan Redl <bernhardlaus@gmx.at>
2025-04-10 20:07:39 +0200

    now its possible to add a note to the firestore database

--- src/app/add-note-dialog/add-note-dialog.component.ts
+++ src/app/add-note-dialog/add-note-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, Input } from '@angular/core';
 import { Note } from '../interfaces/note.interface';
 import { NoteListService } from '../firebase-services/note-list.service'
 import { CommonModule } from '@angular/common';
@@ -16,7 +16,7 @@ export class AddNoteDialogComponent {
   title = "";
   description = "";
 
-  constructor(public noteService: NoteListService){}
+  constructor(private noteService: NoteListService){}
 
   closeDialog() {
     this.title = "";
@@ -25,7 +25,14 @@ export class AddNoteDialogComponent {
   }
 
   addNote(){
-    //beachte das closeDialog() zum Schluss kommt, denn es leert die Variablen
+    let note:Note = {
+      id: "",
+      type: "note",
+      title: this.title,
+      content: this.description,
+      marked: false,
+    }
+    this.noteService.addNote(note)
     this.closeDialog();
   }
 }
